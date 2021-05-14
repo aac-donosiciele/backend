7778634f1697a9ecf28fcd1395488c944c67170b
Maciej Chlebny <chlebny59@gmail.com>
2021-05-14 11:54:07 +0200

    start

---
+++ DonosServer/Infrastructure1/Donos_Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Infrastructure
+{
+    public class Donos_Context : DbContext
+    {
+        public Donos_Context(DbContextOptions<Donos_Context> options) : base(options)
+        {
+        }
+
+        protected Donos_Context()
+        {
+        }
+
+
+    }
+}
