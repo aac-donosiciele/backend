638a78de5bb1fa6a0d1f77649dd1fd493bc96363
Marcin Galiński <marcing305@wp.pl>
2021-05-28 01:54:38 +0200

    implement UsersController

--- DonosServer/Controllers/UsersController.cs
+++ DonosServer/Controllers/UsersController.cs
@@ -1,8 +1,11 @@
-﻿using DonosServer.API.DTOs.Requests;
+﻿using Core.Entities;
+using Core.Interfaces;
+using DonosServer.API.DTOs.Requests;
 using DonosServer.API.DTOs.Responses;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DonosServer.API.Controllers
 {
@@ -10,34 +13,90 @@ namespace DonosServer.API.Controllers
     [Route("/users")]
     public class UsersController : ControllerBase
     {
+        private readonly IUserService userService;
+        private readonly IComplaintService complaintService;
+        private readonly IComplaintLogService complaintLogService;
+
+        public UsersController(IUserService userService, IComplaintService complaintService, IComplaintLogService complaintLogService)
+        {
+            this.userService = userService;
+            this.complaintService = complaintService;
+            this.complaintLogService = complaintLogService;
+        }
+        
         [HttpPut]
         public IActionResult EditUser(EditUserRequest request)
         {
-            throw new NotImplementedException();
+            var user = userService.Get(new Guid(request.Id));
+            user.Pesel = request.Pesel;
+            user.IsVerified = request.Verified;
+            userService.Edit(user);
+            return Ok();
         }
 
         [HttpGet("{id}")]
         public ActionResult<GetUserResponse> GetUser(string id)
         {
-            throw new NotImplementedException();
+            var user = userService.Get(new Guid(id));
+            return user switch
+            {
+                null => NotFound("User with given ID not found"),
+                _ => Ok(new GetUserResponse
+                {
+                    Id = user.Id.ToString(),
+                    Pesel = user.Pesel,
+                    Verified = user.IsVerified
+                })
+            };
         }
 
         [HttpPost("complaints")]
         public IActionResult CreateComplaint(CreateComplaintRequest request)
         {
-            throw new NotImplementedException();
+            complaintService.Add(new Complaint
+            {
+                Category = request.Category,
+                Note = request.Note,
+                CreatedDate = DateTime.Now,
+                SenderId = new Guid(request.SenderId),
+                SendTime = DateTime.Now,
+                LastModifiedDate = DateTime.Now,
+                TargetFirstName = request.TargetFirstName,
+                TargetLastName = request.TargetLastName,
+                TargetAddress = request.TargetAddress
+            });
+            return Ok();
         }
 
         [HttpGet("{id}/complaints")]
         public ActionResult<IEnumerable<GetUserComplaintResponse>> GetUserComplaints(string id)
         {
-            throw new NotImplementedException();
+            var user = userService.Get(new Guid(id));
+            return user switch
+            {
+                null => NotFound("User with given ID not found"),
+                _ => Ok(user.Complaints.Select(x => new GetUserComplaintResponse
+                {
+                    Category = x.Category,
+                    Id = x.Id.ToString(),
+                    Note = x.Note,
+                    SendDate = x.SendTime,
+                    Status = x.ComplaintLogs.OrderByDescending(y => y.UpdateTime).First().Status,
+                    TargetAddress = x.TargetAddress,
+                    TargetFirstName = x.TargetFirstName,
+                    TargetLastName = x.TargetLastName
+                }))
+            };
         }
 
         [HttpDelete("complaints/{id}")]
         public IActionResult CancelComplaint(string id)
         {
-            throw new NotImplementedException();
+            var complaint = complaintService.Get(new Guid(id));
+            if (complaint is null)
+                return NotFound("Complaint with given ID not found");
+            complaintLogService.CancelComplaint(new Guid(id));
+            return Ok();
         }
     }
 }
