c2f5b45e951aa8b4b319f32a30ab079ebe6dada2
Marcin Galiński <marcing305@wp.pl>
2021-05-28 01:43:07 +0200

    implement OfficialsController

--- DonosServer/Controllers/OfficialsController.cs
+++ DonosServer/Controllers/OfficialsController.cs
@@ -1,8 +1,11 @@
-﻿using DonosServer.API.DTOs.Requests;
+﻿using Core.Entities;
+using Core.Interfaces;
+using DonosServer.API.DTOs.Requests;
 using DonosServer.API.DTOs.Responses;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DonosServer.API.Controllers
 {
@@ -10,40 +13,118 @@ namespace DonosServer.API.Controllers
     [Route("/officials")]
     public class OfficialsController : ControllerBase
     {
+        private readonly IOfficialService officialService;
+        private readonly IComplaintService complaintService;
+        private readonly IAuthorityService authorityService;
+
+        public OfficialsController(IOfficialService officialService, IComplaintService complaintService, IAuthorityService authorityService)
+        {
+            this.officialService = officialService;
+            this.complaintService = complaintService;
+            this.authorityService = authorityService;
+        }
+        
         [HttpPost]
         public IActionResult AddOfficial(AddOfficialRequest request)
         {
-            throw new NotImplementedException();
+            officialService.Add(new Official
+            {
+                Address = request.Address,
+                Email = request.Email,
+                Id = new Guid(request.Id),
+                Pesel = request.Pesel,
+                Role = request.Role,
+                AuthorityId = new Guid(request.AuthorityId),
+                CreatedDate = DateTime.Now,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                PhoneNumber = request.PhoneNumber,
+                LastModifiedDate = DateTime.Now
+            });
+            return Ok();
         }
 
         [HttpDelete("{id}")]
         public IActionResult RemoveOfficial(string id)
         {
-            throw new NotImplementedException();
+            officialService.Remove(new Guid(id));
+            return Ok();
         }
 
         [HttpPut]
         public IActionResult EditOfficial(EditOfficialRequest request)
         {
-            throw new NotImplementedException();
+            officialService.Edit(new Official
+            {
+                Address = request.Address,
+                Email = request.Email,
+                Id = new Guid(request.Id),
+                Pesel = request.Pesel,
+                Role = request.Role,
+                AuthorityId = new Guid(request.AuthorityId),
+                CreatedDate = DateTime.Now,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                PhoneNumber = request.PhoneNumber,
+                LastModifiedDate = DateTime.Now
+            });
+            return Ok();
         }
 
         [HttpGet("{id}")]
         public ActionResult<GetOfficialResponse> GetOfficial(string id)
         {
-            throw new NotImplementedException();
+            var official = officialService.Get(new Guid(id));
+            return official switch
+            {
+                null => NotFound("Official with given ID not found"),
+                _ => Ok(new GetOfficialResponse
+                {
+                    Address = official.Address,
+                    Email = official.Email,
+                    Id = official.Id.ToString(),
+                    Pesel = official.Pesel,
+                    Role = official.Role,
+                    AuthorityId = official.AuthorityId.ToString(),
+                    FirstName = official.FirstName,
+                    LastName = official.LastName,
+                    PhoneNumber = official.PhoneNumber
+                })
+            };
+
         }
 
         [HttpGet("{id}/complaints")]
         public ActionResult<IEnumerable<GetOfficialComplaintResponse>> GetOfficialComplaints(string id)
         {
-            throw new NotImplementedException();
+            var official = officialService.Get(new Guid(id));
+            return official switch
+            {
+                null => NotFound("Official with given ID not found"),
+                _ => Ok(complaintService.GetOfficialComplaints(official.Id))
+            };
         }
 
         [HttpGet("/authority/{id}/officials")]
         public ActionResult<IEnumerable<GetOfficialResponse>> GetOfficials(string id)
         {
-            throw new NotImplementedException();
+            var authority = authorityService.Get(new Guid(id));
+            return authority switch
+            {
+                null => NotFound("Authority with the given ID not found"),
+                _ => Ok(authority.Officials.Select(x => new GetOfficialResponse
+                {
+                    Address = x.Address,
+                    Email = x.Email,
+                    Id = x.Id.ToString(),
+                    Pesel = x.Pesel,
+                    Role = x.Role,
+                    AuthorityId = x.AuthorityId.ToString(),
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    PhoneNumber = x.PhoneNumber
+                }))
+            };
         }
 
         [HttpPost("/officialComplaint")]
--- DonosServer/DTOs/Requests/EditOfficialRequest.cs
+++ DonosServer/DTOs/Requests/EditOfficialRequest.cs
@@ -8,6 +8,7 @@ namespace DonosServer.API.DTOs.Requests
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Pesel { get; set; }
         public string AuthorityId { get; set; }
